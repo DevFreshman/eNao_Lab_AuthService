ce527ecfcb53c8e432e70e9fd31cf61d6c2f8bc8
Hoàng Đức Mạnh <hoangducmanh2004789@gmail.com>
2026-09-22 16:23:52 +0700

    change API

--- src/main/java/org/com/lab/controller/AuthController.java
+++ src/main/java/org/com/lab/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/public/auth")
+@RequestMapping("/public")
 public class AuthController {
 
     private final AuthServices authServices;
